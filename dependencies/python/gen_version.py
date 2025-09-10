9b14de14c4863c1a0df826fa07477180fc9832b5
Dark <dark@dark.ski>
2025-09-10 20:55:30 +0000

    hide commit hash from motd

--- dependencies/python/gen_version.py
+++ dependencies/python/gen_version.py
@@ -16,9 +16,9 @@ path = sys.argv[1]
 
 f = open(path, "w")
 
-f.write(f'(message_motd "Rock Band 3 Deluxe {version} Loaded! Thanks for playing!")\n')
-f.write(f'(message_motd_signin "Rock Band 3 Deluxe {version} Loaded! Thanks for playing!")\n')
-f.write(f'(message_motd_noconnection "Rock Band 3 Deluxe {version} Loaded! Thanks for playing!")\n')
+f.write(f'(message_motd "Rock Band 3 Deluxe r{rev} Loaded! Thanks for playing!")\n')
+f.write(f'(message_motd_signin "Rock Band 3 Deluxe r{rev} Loaded! Thanks for playing!")\n')
+f.write(f'(message_motd_noconnection "Rock Band 3 Deluxe r{rev} Loaded! Thanks for playing!")\n')
 f.write(f'(rb3e_mod_string "RB3DX {version}")\n')
 
 f.close()
